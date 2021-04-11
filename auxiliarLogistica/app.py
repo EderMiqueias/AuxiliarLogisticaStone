1b205705460f84008f191f97d44c1d540bdcc9ab
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2021-04-10 22:55:59 -0300

    middleware to accept CORS request was added

--- auxiliarLogistica/app.py
+++ auxiliarLogistica/app.py
@@ -1,10 +1,12 @@
 import falcon
+from falcon_cors import CORS
 
 from auxiliarLogistica.api.resources import IndexResource,\
     PolosResource, PoloEstoqueResource
 
+cors = CORS(allow_all_origins=True)
 
-application = falcon.API()
+application = falcon.API(middleware=[cors.middleware])
 polos = PolosResource()
 estoque = PoloEstoqueResource()
 index = IndexResource()
